aa528074af0628e55b2d4b5eecb882fb5bf75baa
zachatkinson <zach.atkinson85@gmail.com>
2024-06-25 15:25:47 -0400

    Added graphQuery.ts initial commit

---
+++ src/lib/graphQuery.ts
@@ -0,0 +1,24 @@
+interface gqlParams {
+    query: String;
+    variables?: object;
+}
+export async function graphQuery({query, variables={} }: gqlParams) {
+    const response = await fetch(import.meta.env.WP_GRAPHQL, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + import.meta.env.JWT_TOKEN,
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables,
+        }),
+    })
+    if (!response.ok){
+        console.error(response);
+        return {};
+    }
+
+    const{ data } = await response.json();
+    return data;
+}
